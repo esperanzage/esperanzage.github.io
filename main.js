3414c47974a990c0c2847527dd3d8429d06b5a57
esperanzage <esperanzage@gmail.com>
2021-02-28 00:11:34 +1300

    Dropdowns fixed, layout fixes and added pricing table

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 
+// const dropdownButton = document.getElementById("dpdwn-button");
+const dropdowns = Array.from(document.getElementsByClassName("dropdown-container"));
+// const dropdownArrow = document.getElementById("dropdown-arrow");
+// const dropdownOptions = Array.from(document.getElementsByClassName("dropdown-option"));
+// const dropdownButtonChildren = Array.from(dropdownButton.childNodes);
+// const dropdownContainers = Array.from(getElementsByClassName("dropdown-container"));
 
 
 
 
-const dropdownButton = document.getElementById("dpdwn-button");
-const dropdown = document.getElementById("dropdown");
-const dropdownArrow = document.getElementById("dropdown-arrow");
-const dropdownOptions = Array.from(document.getElementsByClassName("dropdown-option"))
-const dropdownButtonChildren = Array.from(dropdownButton.childNodes)
-const toggleClass = (element, thing) => {
-    element.classList.toggle(thing);
+const toggleClass = (element, className) => {
+    element.classList.toggle(className);
 };
 
 const setElementContent = (target, content) => {
@@ -17,26 +18,39 @@ const setElementContent = (target, content) => {
 
 };
 
-dropdownButton.addEventListener("click", (e) => {
+
+dropdowns.forEach(dropdown => {
+    const dropdownButton = dropdown.querySelector("#dpdwn-button");
+    const dropdownArrow = dropdown.querySelector("#dropdown-arrow");
+    const dropdownOptionContainer = dropdown.querySelector("#dropdown");
+    const dropdownOptions = Array.from(dropdownOptionContainer.querySelectorAll(".dropdown-option"));
+
+   
+    dropdownButton.addEventListener("click", (e) => {
     e.preventDefault();
-    toggleClass(dropdown, 'hidden');
+    toggleClass(dropdownOptionContainer, 'hidden');
     toggleClass(dropdownArrow, 'rotate');
+    });
 
-});
-
-console.log(dropdownOptions);
-    
-
-dropdownOptions.forEach(element => {
-
-    element.addEventListener("click", () => {
+    dropdownOptions.forEach(element => {
+        element.addEventListener("click", () => {
         setElementContent(Array.from(dropdownButton.childNodes)[0], element.textContent);
-        toggleClass(dropdown, 'hidden');
+        toggleClass(dropdownOptionContainer, 'hidden');
         toggleClass(dropdownArrow, 'rotate');
-    })
-    
+        });
+    });
+
+    // TO DO: Move to outside of loop and get currently open dropdowns
+    document.addEventListener("click", (e) => {
+    if (!dropdown.contains(e.target)) {
+        dropdownOptionContainer.classList.add('hidden');
+    };
+ });
+ 
 });
 
+    
 
-console.log(dropdownButton.childNodes);
+
+
 
